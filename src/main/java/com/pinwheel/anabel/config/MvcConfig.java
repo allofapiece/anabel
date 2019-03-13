84cc7530008084512be6575cca6ff37c3313974a
allofapiece <stasana1998@gmail.com>
2019-03-13 23:28:02 +0300

    ANABEL-22: Integrated internationalization.

--- src/main/java/com/pinwheel/anabel/config/MvcConfig.java
+++ src/main/java/com/pinwheel/anabel/config/MvcConfig.java
@@ -1,9 +1,18 @@
 package com.pinwheel.anabel.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * Spring MVC configuration.
@@ -28,5 +37,44 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    /**
+     * Set up and return {@link ReloadableResourceBundleMessageSource} bean. Set up basename as
+     * {@code classpath:i18n/messages}.
+     *
+     * @return implementation of {@link MessageSource}.
+     */
+    @Bean
+    public MessageSource messageSource() {
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    /**
+     * Set up and return {@link SessionLocaleResolver} bean. Set up default locale as {@code ENGLISH}.
+     *
+     * @return {@link LocaleResolver} bean.
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        final SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+
+        return sessionLocaleResolver;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        final LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
--- src/main/java/com/pinwheel/anabel/controller/HomeController.java
+++ src/main/java/com/pinwheel/anabel/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.pinwheel.anabel.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Locale;
 
 /**
  * Home controller. Describes home pages.
@@ -20,5 +23,23 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    /**
+     * Uses for switching languages. Though spring framework is able to intercept request and analyze necessary locale,
+     * Spring MVC and Spring Security is separated parts of framework and default implementation of
+     * {@link org.springframework.web.servlet.i18n.LocaleChangeInterceptor LocaleChangeInterceptor} is not able to
+     * intercept requests for controllers, which has been added by {@code addViewControllers} method of
+     * {@link org.springframework.web.servlet.config.annotation.WebMvcConfigurer WebMvcConfigurer}.
+     *
+     * TODO find solution for this problem.
+     *
+     * @param locale current locale for active session.
+     * @return current locale.
+     */
+    @GetMapping("/lang")
+    public @ResponseBody
+    Locale lang(Locale locale) {
+        return locale;
     }
 }
